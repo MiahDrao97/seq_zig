98f279180bff40a79f1e3a6cefaa5edcc9f3c679
Jeremiah D. Oard <64376668+MiahDrao97@users.noreply.github.com>
2026-02-17 23:10:08 -0700

    welp, it didnt work in release modes, so I had to take the approach of my own logging API

---
+++ src/log.zig
@@ -0,0 +1,59 @@
+//! Almost a mirror of `std.log` except with 2 more log levels (reflecting Seq's log levels)
+//! and requiring a source location as the first parameter.
+//!
+//! Still calls the log function in `std.log` with the given scope and translated log level.
+
+/// Declare a scoped log
+pub fn scoped(scope: @EnumLiteral()) type {
+    return struct {
+        pub fn verbose(comptime src: SourceLocation, comptime format: []const u8, args: anytype) void {
+            seq_zig.seqLogFn(src, .Verbose, scope, format, args);
+        }
+
+        pub fn debug(comptime src: SourceLocation, comptime format: []const u8, args: anytype) void {
+            seq_zig.seqLogFn(src, .Debug, scope, format, args);
+        }
+
+        pub fn info(comptime src: SourceLocation, comptime format: []const u8, args: anytype) void {
+            seq_zig.seqLogFn(src, .Information, scope, format, args);
+        }
+
+        pub fn warn(comptime src: SourceLocation, comptime format: []const u8, args: anytype) void {
+            seq_zig.seqLogFn(src, .Warning, scope, format, args);
+        }
+
+        pub fn err(comptime src: SourceLocation, comptime format: []const u8, args: anytype) void {
+            seq_zig.seqLogFn(src, .Error, scope, format, args);
+        }
+
+        pub fn fatal(comptime src: SourceLocation, comptime format: []const u8, args: anytype) void {
+            seq_zig.seqLogFn(src, .Fatal, scope, format, args);
+        }
+    };
+}
+
+/// Default log scope
+pub const default = scoped(.default);
+
+/// Call verbose logging with the default scope
+pub const verbose = default.verbose;
+
+/// Call debug logging with the default scope
+pub const debug = default.debug;
+
+/// Call info logging with the default scope
+pub const info = default.info;
+
+/// Call warn logging with the default scope
+pub const warn = default.warn;
+
+/// Call error logging with the default scope
+pub const err = default.err;
+
+/// Call fatal logging with the default scope
+pub const fatal = default.fatal;
+
+const std = @import("std");
+const seq_zig = @import("root.zig");
+const SourceLocation = std.builtin.SourceLocation;
+const SeqLogLevel = seq_zig.SeqLogLevel;
